880f402d58080c58e1d6a811efcc0f2026244151
ksj970714 <ksj970714@gmail.com>
2022-11-21 14:59:59 +0900

    결과창 영화 추천 기능 및 스코어보드 구성

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+# 유효성 검증 중, 최소/최댓값에 대한 검증을 하는 validator
 
 class movie(models.Model):
     title = models.CharField(max_length=100,unique=True)
@@ -10,7 +12,7 @@ class movie(models.Model):
 
 class Scoreboard(models.Model):
     name = models.CharField(max_length=20)
-    score = models.IntegerField()
+    score = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(30)])
 
 
 
