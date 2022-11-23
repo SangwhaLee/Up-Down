f3fbd962a4d1164e52f693204aa2bf74a5383fe9
ksj970714 <ksj970714@gmail.com>
2022-11-23 11:38:30 +0900

    스코어보드 form

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'myapp'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('gameover/<int:score>', views.gameover, name='gameover'),
